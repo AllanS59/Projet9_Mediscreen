47c1e31d9342bac72ceff9a6b68e919f5e631267
Allan Saudemont <allan.saudemont@gmail.com>
2023-04-24 14:57:22 +0200

    unit tests for controller added

--- mediscreenPatient/src/main/java/com/openclassrooms/mediscreenPatient/controller/PatientController.java
+++ mediscreenPatient/src/main/java/com/openclassrooms/mediscreenPatient/controller/PatientController.java
@@ -1,80 +1,55 @@
 package com.openclassrooms.mediscreenPatient.controller;
 
+import com.google.gson.Gson;
 import com.openclassrooms.mediscreenPatient.model.Patient;
 import com.openclassrooms.mediscreenPatient.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import javax.validation.Valid;
 
-@Controller
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class PatientController {
 
     @Autowired
     private PatientService patientService;
 
 
-    @RequestMapping("/patient/list")
-    public String home(Model model) {
+    @GetMapping("/patients")
+    public String getPatients() {
         List<Patient> listPatients = patientService.getAllPatients();
-        model.addAttribute("listPatients", listPatients);
-        return "patientList";
+        String json = new Gson().toJson(listPatients);
+        return json;
     }
 
+    @GetMapping("/patient")
+    public String getPatientById(@RequestParam(name = "patientId") int patientId) {
 
-    @GetMapping("/patient/add")
-    public String addpatientForm(Patient patient) {
-        return "patientAdd";
+        Optional<Patient> patientById = patientService.getPatientById(patientId);
+        String json = new Gson().toJson(patientById.get());
+        return json;
     }
 
-    @PostMapping("/patient/validate")
-    public String validate(@Valid @DateTimeFormat(pattern= "yyyy-MM-dd") Patient patient, BindingResult result, Model model) {
-        // If no errors in data provided by user, save data and go back to 'list' page
-        if (!result.hasErrors()) {
-            patientService.addPatient(patient);
-            List<Patient> listPatients = patientService.getAllPatients();
-            model.addAttribute("listPatients", listPatients);
-            return "redirect:/patient/list";
-        }
-        // else stay on the current page
-        return "patientAdd";
-    }
-
-
-    @GetMapping("/patient/update/{id}")
-    public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        Patient patient = patientService.getPatientById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid Patient Id:" + id));
-        model.addAttribute("patient", patient);
-        return "patientUpdate";
-    }
-
-
-    @PostMapping("/patient/update/{id}")
-    public String updatePatient(@PathVariable("id") Integer id, @Valid Patient patient, BindingResult result, Model model) {
-        // If errors in data provided by user, stay on current page
-        if (result.hasErrors()) {
-            return "patientUpdate";
-        }
-        // If no error, save data into database and go back to 'List' page
-        patient.setPatientId(id);
+    @PostMapping("/patient")
+    public Patient postPatient(@RequestBody Patient patient) {
         patientService.addPatient(patient);
-        List<Patient> listPatients = patientService.getAllPatients();
-        model.addAttribute("listPatients", listPatients);
-        return "redirect:/patient/list";
+        return patient;
     }
 
 
+    @PutMapping("/patient")
+    public Patient updatePatient(@RequestBody Patient patient) {
+        patientService.updatePatient(patient);
+        return patient;
+    }
 
-    @GetMapping("/patient/delete/{id}")
-    public String deletePatient(@PathVariable("id") Integer id, Model model) {
-        patientService.deletePatientById(id);
-        return "redirect:/patient/list";
+
+    @DeleteMapping("/patient")
+    public void deletePersonById(@RequestParam(name = "patientId") int patientId) {
+        patientService.deletePatientById(patientId);
     }
 
 }
+
---
+++ mediscreenPatient/src/test/java/com/openclassrooms/mediscreenPatient/controller/PatientWebControllerTests.java
@@ -0,0 +1,50 @@
+package com.openclassrooms.mediscreenPatient.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PatientWebControllerTests {
+
+    @Autowired
+	public MockMvc mockMvc;
+
+    @Test
+	public void patientListPageTest() throws Exception {
+        mockMvc.perform(get("/web/patient/list"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("patientList"));
+    }
+
+    @Test
+    public void patientAddPageTest() throws Exception {
+        mockMvc.perform(get("/web/patient/add"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("patientAdd"));
+    }
+
+    @Test
+    public void patientUpdatePageTest() throws Exception {
+        mockMvc.perform(get("/web/patient/update/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("patientUpdate"));
+    }
+
+
+}
